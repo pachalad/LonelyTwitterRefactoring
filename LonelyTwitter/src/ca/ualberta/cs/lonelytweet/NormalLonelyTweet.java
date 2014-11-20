6305209abed0d5fa617c8df0b2ca4c96177aca50
pachalad <pachala@ualberta.ca>
2014-11-19 18:14:12 -0700

    Last lab!

---
+++ LonelyTwitter/src/ca/ualberta/cs/lonelytweet/NormalLonelyTweet.java
@@ -0,0 +1,40 @@
+package ca.ualberta.cs.lonelytweet;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class NormalLonelyTweet extends 
+LonelyTweet implements Serializable {
+
+	public NormalLonelyTweet() {
+	}
+
+	public NormalLonelyTweet(String text) {
+		this.tweetDate = new Date();
+		this.tweetBody = text;
+	}
+
+	public void setTweetBody(String tweetBody) {
+		this.tweetBody = tweetBody;
+	}
+
+	@Override
+	public boolean isValid() {
+		if (tweetBody.trim().length() == 0
+				|| tweetBody.trim().length() > 10) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return getTweetDate() + " | " + getTweetBody() ;
+	}
+
+	public String getTweetBody()
+	{
+		return tweetBody;
+	}
+}
